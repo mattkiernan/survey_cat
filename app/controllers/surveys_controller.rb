8a408f7217542d8d9ff1bca8d3981d32ba83e01f
mattkiernan <matt.kiernan@gmail.com>
2014-11-15 14:53:26 -0500

    Users can select survey from index and view it

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -4,4 +4,31 @@ class SurveysController < ApplicationController
   def index
     @surveys = current_user.surveys
   end
+
+  def new
+    @survey = Survey.new
+  end
+
+  def create
+    @survey = Survey.new(survey_params)
+    if @survey.save
+      redirect_to @survey
+    else
+      render :new
+    end
+  end
+
+  def show
+    @survey = load_survey_from_url
+  end
+
+  private
+
+  def survey_params
+    params.require(:survey).permit(:body, :description)
+  end
+
+  def load_survey_from_url
+    Survey.find(params[:id])
+  end
 end
